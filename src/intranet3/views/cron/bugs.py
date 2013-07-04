c5af40129b6d26ad6c2b6b84a972e8730c886a9a
Tomasz Modrzyński <tomasz.modrzynski@stxnext.pl>
2013-07-04 10:47:53 +0200

    Reorganized code. Getting all bugs.

--- src/intranet3/views/cron/bugs.py
+++ src/intranet3/views/cron/bugs.py
@@ -1,62 +1,50 @@
 # -*- coding: utf-8 -*-
-import datetime
-
 from pyramid.view import view_config
 from pyramid.response import Response
 from pyramid.renderers import render
 
 from intranet3 import config
-from intranet3.utils.views import CronView
-from intranet3.models import User, Project
-from intranet3.log import INFO_LOG, DEBUG_LOG, EXCEPTION_LOG
-from intranet3.utils.mail import EmailSender
 from intranet3.lib.bugs import Bugs
+from intranet3.log import INFO_LOG, DEBUG_LOG, EXCEPTION_LOG
+from intranet3.models import User, Project
+from intranet3.utils.mail import EmailSender
+from intranet3.utils.views import CronView
+
 
 LOG = INFO_LOG(__name__)
 DEBUG = DEBUG_LOG(__name__)
 EXCEPTION = EXCEPTION_LOG(__name__)
 
+
 @view_config(route_name='cron_bugs_oldbugsreport', permission='cron')
 class OldBugsReport(CronView):
 
-    def _prepare_bugs(self, user, bugs):
-        for bug in Bugs(self.request, user).get_user():
-            if not hasattr(bug, 'project') or not hasattr(bug.project, 'coordinator_id'):
-                continue
-            coordinator_id = bug.project.coordinator_id
-            if coordinator_id not in bugs:
-                bugs[coordinator_id] = [[], []]
-
-            bug_id = bug.tracker.id, bug.tracker.name, bug.id
-            if bug_id not in bugs[coordinator_id][0]:
-                bugs[coordinator_id][0].append(bug_id)
-                bugs[coordinator_id][1].append(bug)
-
-            if bug_id not in bugs['__all__'][0]:
-                bugs['__all__'][0].append(bug_id)
-                bugs['__all__'][1].append(bug)
-
-        bugs = dict(
-            (k, [v[0], sorted(v[1], key=lambda b: b.changeddate.replace(tzinfo=None))])
-            for k, v in bugs.items()
-        )
-
-        return bugs
-
-    def _send_report(self, coordinator, email, bugs):
+    def _send_report(self, coordinator_id, email, bugs):
         def on_success(result):
             LOG(u'Monthly report with old bugs - sent')
         def on_error(err):
             EXCEPTION(u'Failed to sent Monthly report with old bugs')
 
-        if coordinator in bugs:
-            if coordinator == '__all__':
-                title = u'Lista najstarszych niezamkniętych bugów\nwe wszystkich projektach'
-            else:
-                title = u'Lista najstarszych niezamkniętych bugów\nw projektach w których jesteś koordynatorem'
-            data = {'bugs': bugs[coordinator][1][:20],
-                    'title': self._(title)}
-
+        # Bugs filtering & ordering
+        # Coordinator gets bugs from his projects, manager gets bugs from
+        # all projects
+        if coordinator_id is None: # Manager
+            bugs_filtered = sorted(
+                bugs,
+                key=lambda b: b.changeddate.replace(tzinfo=None),
+            )
+            title = u'Lista najstarszych niezamkniętych bugów\nwe wszystkich projektach'
+        else: # Coordinator
+            bugs_filtered = sorted(
+                [b for b in bugs if b.project.coordinator_id == coordinator_id],
+                key=lambda b: b.changeddate.replace(tzinfo=None),
+            )
+            title = u'Lista najstarszych niezamkniętych bugów\nw projektach w których jesteś koordynatorem'
+        if bugs_filtered:
+            data = {
+                'bugs': bugs_filtered[:20],
+                'title': self._(title),
+            }
             response = render(
                 'intranet3:templates/_email_reports/old_bugs_report.html',
                 data,
@@ -64,30 +52,26 @@ class OldBugsReport(CronView):
             )
             deferred = EmailSender.send_html(
                 email,
-                self._(u'[Intranet2] Old bugs report'),
+                self._(u'[Intranet3] Old bugs report'),
                 response
             )
             deferred.addCallbacks(on_success, on_error)
 
     def action(self):
-        coordinators = self.session.query(Project.coordinator_id, User.email, User.name, User)\
-                                   .join(User)\
-                                   .filter(Project.coordinator_id!=None)\
-                                   .group_by(Project.coordinator_id, User)\
+        coordinators = self.session.query(Project.coordinator_id, User.email) \
+                                   .join(User) \
+                                   .filter(Project.coordinator_id!=None) \
+                                   .group_by(Project.coordinator_id, User) \
                                    .all()
-        # prepare bugs
-        bugs = {'__all__': [[], []]}
-        for user_id, user_email, user_name, user in coordinators:
-            bugs = self._prepare_bugs(user, bugs)
+        manager = self.session.query(User) \
+                              .filter(User.email == config['MANAGER_EMAIL']) \
+                              .first()
+        bugs = Bugs(self.request, manager).get_all()
 
-        # send report
-        for user_id, user_email, user_name, user in coordinators:
+        # Coordinators
+        for user_id, user_email in coordinators:
             self._send_report(user_id, user_email, bugs)
-
-        self._send_report('__all__', config['MANAGER_EMAIL'], bugs)
+        # Manager
+        self._send_report(None, config['MANAGER_EMAIL'], bugs)
 
         return Response('ok')
-
-
-
-
