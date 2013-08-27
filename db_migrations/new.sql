1dbb8dfbdad5f7759e34d48764f31758ca32ec9b
meverone <meverone@gmail.com>
2013-08-27 11:11:28 +0200

    db migrations

--- db_migrations/new.sql
+++ db_migrations/new.sql
@@ -8,3 +8,11 @@ FROM sprint s2;
 
 ALTER TABLE sprint ALTER COLUMN bugs_project_ids TYPE INTEGER[] USING array[project_id]::INTEGER[];
 -- add bugs_project_ids
+-- add github to tracker.type
+BEGIN;
+ALTER type tracker_type_enum RENAME to old__tracker_type_enum;
+CREATE type tracker_type_enum as enum ('bugzilla', 'trac', 'cookie_trac', 'igozilla', 'bitbucket', 'rockzilla', 'pivotaltracker', 'harvest', 'unfuddle', 'github');
+ALTER TABLE tracker ALTER COLUMN type TYPE tracker_type_enum USING type::text::tracker_type_enum;
+DROP type old__tracker_type_enum;
+COMMIT;
+-- add github to tracker.type
