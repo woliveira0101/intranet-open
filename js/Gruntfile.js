12903152f6088a91c3730cb2a493d2d2dbd0060a
Konrad Rotkiewicz <konrad.rotkiewicz@stxnext.pl>
2013-08-28 18:44:36 +0200

    grunt ready

---
+++ js/Gruntfile.js
@@ -0,0 +1,103 @@
+var FILES = require('./assets.js');
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    clean: {
+      options: {
+        force: true
+      },
+      src: ['build/*']
+    },
+    ngmin: {
+      app: {
+        src: FILES['APP_JS'],
+        dest: FILES['NGMINNED_APP']
+      }
+    },
+    concat: {
+      vendor_js: {
+        options: {
+          separator: '\n;\n'
+        },
+        src: FILES['VENDOR_JS'],
+        dest: 'build/static/js/vendor.js'
+      },
+      app_js: {
+        src: FILES['APP_JS'],
+        dest: 'build/static/js/app.js'
+      }
+    },
+    uglify: {
+      options: {
+        compress: {}
+      },
+      vendor: {
+        src: FILES['VENDOR_JS'],
+        dest: 'build/static/js/vendor.js'
+      },
+      app: {
+        // we have to run ngmin first
+        src: FILES['NGMINNED_APP'],
+        dest: 'build/static/js/app.js'
+      }
+    },
+    recess: {
+      concat: {
+        options: {
+          compile: true
+        },
+        files: {
+          'build/static/css/app.css': FILES['APP_LESS']
+        }
+      },
+      // recess:compress minifies the resulting css, too - production
+      compress: {
+        options: {
+          compile: true,
+          compress: true
+        },
+        files: {
+          'build/static/css/app.css': FILES['APP_LESS']
+        }
+      }
+    },
+    watch: {
+      js: {
+        files: FILES['APP_JS'],
+        tasks: 'concat'
+      },
+      less: {
+        files: FILES['APP_LESS'],
+        tasks: 'recess:concat'
+      }
+    },
+    ngtemplates: {
+      myapp: {
+         options:    {
+           htmlmin: {}
+         },
+         src: 'assets/partials/*.html',
+         dest: FILES['PARTIALS']
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+ // Grunt modules
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-ngmin');
+  grunt.loadNpmTasks('grunt-recess');
+  grunt.loadNpmTasks('grunt-angular-templates');
+
+  // Default task(s).
+  grunt.registerTask('dev', ['clean', 'ngtemplates', 'concat', 'recess:concat', 'watch']);
+  grunt.registerTask('prod', ['clean', 'ngtemplates', 'ngmin', 'recess:compress', 'uglify']);
+
+};
---
+++ js/assets.js
@@ -0,0 +1,34 @@
+var BOWER_JS = [
+  'bower_components/jquery/jquery.min.js',
+  'bower_components/jquery-ui/ui/jquery-ui.js',
+  'bower_components/bootstrap/js/*.js',
+  'bower_components/angular/angular.js',
+  'bower_components/angular-bootstrap/ui-bootstrap.min.js',
+  'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
+  'bower_components/datejs/build/date-en-US.js',
+  'bower_components/modernizr/modernizr.js',
+  'bower_components/underscore/underscore-min.js',
+  'bower_components/spectrum/spectrum.js'
+];
+
+var PARTIALS = 'build/static/js/partials.js';
+
+module.exports = {
+   // Other vendors
+  VENDOR_JS: BOWER_JS.concat([
+    'assets/js/vendor/*.js'
+  ]),
+  VENDOR_LESS: [
+    'bower_components/bootstrap/less/bootstrap.less'
+  ],
+   // Our things
+  APP_JS: [
+    'assets/js/src/**/*.js',
+    PARTIALS
+  ],
+  APP_LESS: [
+    'assets/less/*.less'
+  ],
+  NGMINNED_APP: 'build/ngminned/app.js',
+  PARTIALS: PARTIALS
+};
