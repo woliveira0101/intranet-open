b7f25633f9039e441fde7dd02c0684cff830dca4
tul <tul@tul-stxNext.(none)>
2013-10-08 08:38:13 +0200

    tracker dla jiry - wyciaganie tylko niezbednych pol

--- src/intranet3/asyncfetchers/jira.py
+++ src/intranet3/asyncfetchers/jira.py
@@ -5,9 +5,9 @@ from xml.etree import ElementTree as ET
 
 from pyramid.decorator import reify
 
-from intranet3.asyncfetchers.base import ( BaseFetcher, CSVParserMixin,
-    SimpleProtocol, BasicAuthMixin,
-    FetchException, Bug, cached_bug_fetcher )
+from intranet3.asyncfetchers.base import (BaseFetcher, CSVParserMixin,
+                                          SimpleProtocol, BasicAuthMixin,
+                                          FetchException, Bug, cached_bug_fetcher)
 from intranet3 import helpers as h
 from intranet3.log import EXCEPTION_LOG, INFO_LOG
 
@@ -20,7 +20,6 @@ class JiraBug(Bug):
 
     def get_url(self, number=None):
         number = number if number else self.id
-        # return self.tracker.url + '/browse/%(id)s' % {'id': number}
         return self.tracker.url + '/ViewIssue.jspa?id=%(id)s' % {'id': number}
 
     def is_unassigned(self):
@@ -51,19 +50,24 @@ def _fetcher_function(resolved, single):
         if single:
             jql_params.append('assignee=' + self.login)
         jql = '+AND+'.join(jql_params)
-        url = '%(url)s/rest/api/2/search?jql=%(jql)s' % \
-            {'url': self.tracker.url, 'jql': jql }
+        url = '%(url)s/rest/api/2/search?jql=%(jql)s&fields=%(fields)s' % \
+            {'url': self.tracker.url,
+             'jql': jql,
+             'fields': self.get_fields_list()}
         self.fetch_get(url)
     return fetcher
+
 
 def _query_fetcher_function(resolved=False, all=False):
     def fetcher(self, ticket_ids, project_selector, component_selector,
                 version):
         jql_params = ['project=' + project_selector]
         if component_selector:
-            jql_params.append(build_query_alternative('component', component_selector))
+            jql_params.append(build_query_alternative('component',
+                                                      component_selector))
         if version:
-            jql_params.append(build_query_alternative('affectedVersion', version))
+            jql_params.append(build_query_alternative('affectedVersion',
+                                                      version))
         if ticket_ids:
             jql_params.append(build_query_alternative('id', ticket_ids))
 
@@ -74,10 +78,13 @@ def _query_fetcher_function(resolved=False, all=False):
                 jql_params.append('status!=done')
 
         jql = '+AND+'.join(jql_params)
-        url = '%(url)s/rest/api/2/search?jql=%(jql)s' % \
-            {'url': self.tracker.url, 'jql': jql }
+        url = '%(url)s/rest/api/2/search?jql=%(jql)s&fields=%(fields)s' % \
+            {'url': self.tracker.url,
+             'jql': jql,
+             'fields': self.get_fields_list()}
         self.fetch_get(url)
     return fetcher
+
 
 def build_query_alternative(field, data):
     params = []
@@ -87,37 +94,50 @@ def build_query_alternative(field, data):
 
 
 def get_blocked(d):
-    links = filter(lambda l: l['type']['id'] == '10000' and 'outwardIssue' in l, d['fields']['issuelinks'])
+    links = filter(
+        lambda l: l['type']['id'] == '10000' and 'outwardIssue' in l,
+        d['fields']['issuelinks'])
     links = map(lambda l: l['outwardIssue'], links)
-    return dict(map(lambda t: (t['id'], {'desc': t['fields']['summary'], 'resolved': t['fields']['status']['id'] == '10000'}), links))
+    return dict(map(lambda t: (t['id'], {'desc': t['fields']['summary'],
+                                         'resolved': t['fields']['status']['id'] == '10000'}), links))
 
 
 jira_converter = h.Converter(
     id=lambda d: d['id'],
     desc=lambda d: d['fields']['summary'],
     reporter=lambda d: d['fields']['reporter']['name'],
-    owner=lambda d: d['fields']['assignee']['name'] if d['fields']['assignee'] else '',
+    owner=lambda d: d['fields']['assignee']['name'] if d['fields']['assignee']
+        else '',
     priority=lambda d: d['fields']['priority']['name'],
     severity=lambda d: d['fields']['priority']['name'],
     status=lambda d: d['fields']['status']['name'],
     resolution=lambda d: d['fields']['resolution'],
     project_name=lambda d: d['fields']['project']['name'],
-    component_name=lambda d: ', '.join(map(lambda c: c['name'], d['fields']['components'])),
-    deadline=lambda d: parse(d['fields']['duedate']) if d['fields']['duedate'] else '',
+    component_name=lambda d: ', '.join(map(lambda c: c['name'], d['fields']
+                            ['components'])),
+    deadline=lambda d: parse(d['fields']['duedate']) if d['fields']['duedate']
+        else '',
     opendate=lambda d: parse(d['fields']['created']),
-    changeddate=lambda d: parse(d['fields']['updated']) if d['fields']['updated'] else '',
+    changeddate=lambda d: parse(d['fields']['updated']) if d['fields']
+        ['updated'] else '',
     labels=lambda d: d['fields']['labels'],
-    dependson=lambda d: dict(map(lambda t: (t['id'], {'desc': t['fields']['summary'], 'resolved': t['fields']['status']['id'] == '10000'}), d['fields']['subtasks'])),
+    dependson=lambda d: dict(map(lambda t: (t['id'], {'desc': t['fields']
+        ['summary'], 'resolved': t['fields']['status']['id'] == '10000'}),
+        d['fields']['subtasks'])),
     blocked=get_blocked
 )
 
 
 class JiraFetcher(BasicAuthMixin, CSVParserMixin, BaseFetcher):
+
     """ Fetcher for Jira bugs """
 
-    # redirect_support = True
     bug_class = JiraBug
     get_converter = lambda self: jira_converter
+
+    FIELDS = ['summary', 'reporter', 'assignee', 'priority', 'status',
+              'resolution', 'project', 'components', 'duedate', 'created', 'updated',
+              'labels', 'subtasks', 'issuelinks']
 
     def fetch_get(self, url, on_success=None):
         if not on_success:
@@ -134,6 +154,9 @@ class JiraFetcher(BasicAuthMixin, CSVParserMixin, BaseFetcher):
                 tracker=self.tracker,
                 **converter(bug_desc)
             )
+
+    def get_fields_list(self):
+        return ','.join(self.FIELDS)
 
     fetch_user_tickets = _fetcher_function(resolved=False, single=True)
 
