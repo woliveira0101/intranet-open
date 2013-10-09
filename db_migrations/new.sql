74111ad0386ac432632e6bab06100e5726e09e4a
tul <tul@tul-stxNext.(none)>
2013-10-09 14:12:50 +0200

    skrypt migracyjny dodajacy jire dostosowany do wersji 8.4 postgres

--- db_migrations/new.sql
+++ db_migrations/new.sql
@@ -1,4 +1,8 @@
-ALTER TABLE "user" ADD COLUMN roles VARCHAR[] DEFAULT '{}';
-ALTER TABLE "user" ADD COLUMN stop_work DATE DEFAULT NULL;
-
-ALTER TYPE tracker_type_enum ADD VALUE 'jira';
+-- add jira to tracker.type
+BEGIN;
+ALTER type tracker_type_enum RENAME to old__tracker_type_enum;
+CREATE type tracker_type_enum as enum ('bugzilla', 'trac', 'cookie_trac', 'igozilla', 'bitbucket', 'rockzilla', 'pivotaltracker', 'harvest', 'unfuddle', 'github', 'jira');
+ALTER TABLE tracker ALTER COLUMN type TYPE tracker_type_enum USING type::text::tracker_type_enum;
+DROP type old__tracker_type_enum;
+COMMIT;
+-- add jira to tracker.type
