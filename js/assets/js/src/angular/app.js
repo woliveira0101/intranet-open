ab6809ff2528b8a93b8edf76e597367ec15b3283
Konrad Rotkiewicz <konrad.rotkiewicz@stxnext.pl>
2013-11-09 14:16:40 +0100

    post code review fixes

--- js/assets/js/src/angular/app.js
+++ js/assets/js/src/angular/app.js
@@ -1,5 +1,9 @@
 var App = angular.module('intranet', ['ngDragDrop', 'ui.bootstrap', '$strap.directives']);
 
+App.config(function($httpProvider){
+  $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+});
+
 App.run(function($rootScope) {
   $rootScope.G = G;
 });
--- js/assets/js/src/angular/lateness.js
+++ js/assets/js/src/angular/lateness.js
@@ -12,13 +12,7 @@ App.controller('latenessCtrl', function($scope, $http, $dialog, dialog) {
         $scope.form_submitted = true;
 
         $http.post('/api/lateness', {
-            lateness: {
-                popup_date: $scope.lateness.popup_date,
-                popup_explanation: $scope.lateness.popup_explanation,
-                late_start: $scope.lateness.late_start,
-                late_end: $scope.lateness.late_end,
-                work_from_home: $scope.lateness.work_from_home
-            }
+            lateness: $scope.lateness
         }).success(function(data) {
             $scope.close();
 
@@ -26,6 +20,9 @@ App.controller('latenessCtrl', function($scope, $http, $dialog, dialog) {
                 resolve: {messages: function() {return data;}}
             }).open('modalConfirm.html', 'modalConfirmCtrl');
         }).error(function(data) {
+            $scope.latenessForm.$setPristine();
+            $scope.errors = {};
+
             angular.forEach(data, function(errors, field) {
                 $scope.latenessForm[field].$setValidity('server', false);
                 $scope.errors[field] = errors.join('<br/>');
