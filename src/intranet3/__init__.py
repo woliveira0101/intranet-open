bcf06ddc05b8ee33494c0f484e9d786d8ac91749
Konrad Rotkiewicz <konrad.rotkiewicz@stxnext.pl>
2013-12-07 12:05:55 +0100

    move ACLs

--- src/intranet3/__init__.py
+++ src/intranet3/__init__.py
@@ -12,7 +12,6 @@ from pyramid.config import Configurator
 from pyramid_beaker import session_factory_from_settings
 from pyramid.authentication import AuthTktAuthenticationPolicy
 from pyramid.authorization import ACLAuthorizationPolicy
-from pyramid.security import Allow, Authenticated, ALL_PERMISSIONS
 from werkzeug.contrib.cache import MemcachedCache
 
 from twisted.web.wsgi import WSGIResource
@@ -31,74 +30,6 @@ class CustomAuthenticationPolicy(AuthTktAuthenticationPolicy):
         return result
 
 
-class Root(object):
-
-    PERMS = (
-        ('',                          ('freelancer', 'employee', 'coordinator', 'scrum master', 'business', 'hr', 'client',)),
-        ('can_view_users',            ('A',          'A',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
-        ('can_edit_own_profile',      ('A',          'A',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
-        ('can_see_own_bugs',          ('A',          'A',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
-        ('can_add_timeentry',         ('A',          'A',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
-        ('can_justify_wrongtime',     ('A',          'A',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
-        ('can_justify_late',          (' ',          'A',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
-        ('can_view_presence',         (' ',          'A',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
-        ('can_edit_presence',         (' ',          'A',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
-        ('can_view_task_pivot',       (' ',          'A',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
-        ('can_view_teams',            (' ',          'A',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
-        ('can_view_sprints',          (' ',          'A',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
-        ('client_stuff',              (' ',          'A',        ' ',           ' ',            ' ',        ' ',  'A',     )),
-        ('can_edit_sprints',          (' ',          ' ',        'A',           'A',            ' ',        ' ',  ' ',     )),
-        ('can_view_clients',          (' ',          ' ',        'A',           'A',            ' ',        ' ',  ' ',     )),
-        ('can_view_projects',         (' ',          ' ',        'A',           'A',            ' ',        ' ',  ' ',     )),
-        ('can_edit_clients',          (' ',          ' ',        'A',           ' ',            ' ',        ' ',  ' ',     )),
-        ('can_edit_projects',         (' ',          ' ',        'A',           ' ',            ' ',        ' ',  ' ',     )),
-        ('can_edit_teams',            (' ',          ' ',        ' ',           ' ',            ' ',        'A',  ' ',     )),
-        ('can_see_users_times',       (' ',          ' ',        ' ',           ' ',            ' ',        'A',  ' ',     )),
-        ('hr_stuff',                  (' ',          ' ',        ' ',           ' ',            ' ',        'A',  ' ',     )),
-        ('can_see_inactive_users',    (' ',          ' ',        ' ',           ' ',            ' ',        'A',  ' ',     )),
-        ('can_edit_users',            (' ',          ' ',        ' ',           ' ',            ' ',        'A',  ' ',     )),
-        ('can_times_reports',         (' ',          ' ',        ' ',           ' ',            'A',        ' ',  ' ',     )),
-
-        # ADMIN ONLY:
-        ('can_edit_config',           (' ',          ' ',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
-        ('can_edit_trackers',         (' ',          ' ',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
-        ('can_delete_projects',       (' ',          ' ',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
-        ('can_delete_clients',        (' ',          ' ',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
-        ('can_edit_users_timeentry',  (' ',          ' ',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
-    )
-
-    @staticmethod
-    def generate(perms):
-        base = [
-            (Allow, Authenticated, ('view',)),
-            (Allow, 'g:cron', 'cron'),
-            (Allow, 'g:admin', ALL_PERMISSIONS),
-        ]
-        groups = perms[0][1]
-
-        perms = perms[1:]
-
-        result = {group: [] for group in groups}
-        for perm, checklist in perms:
-            for group, check in zip(groups, checklist):
-                # perm, group, check
-                if check == 'A':
-                    result[group].append(perm)
-                elif check != ' ':
-                    raise Exception('%s not supported' % check)
-
-        dynamic = []
-        for group, perms in result.iteritems():
-            dynamic.append((Allow, 'g:%s' % group, perms))
-
-        return base + dynamic
-
-
-    def __init__(self, request):
-        self.request = request
-
-Root.__acl__ = Root.generate(Root.PERMS)
-
 config = None
 memcache = None
 
@@ -113,7 +44,7 @@ def main(global_config, **settings):
     memcache = MemcachedCache([config['MEMCACHE_URI']])
 
     from intranet3.models import DBSession, Base, User
-    from intranet3.utils import request
+    from intranet3.utils import request, acl
     from intranet3.views.auth import forbidden_view
 
     def groupfinder(userid, request):
@@ -139,7 +70,7 @@ def main(global_config, **settings):
         session_factory=session_factory,
         request_factory=request.Request,
         default_permission='view',
-        root_factory=Root,
+        root_factory=acl.Root,
     )
     pyramid_config.add_forbidden_view(forbidden_view)
 
---
+++ src/intranet3/utils/acl.py
@@ -0,0 +1,69 @@
+from pyramid.security import Allow, Authenticated, ALL_PERMISSIONS
+
+class Root(object):
+
+    PERMS = (
+        ('',                          ('freelancer', 'employee', 'coordinator', 'scrum master', 'business', 'hr', 'client',)),
+        ('can_view_users',            ('A',          'A',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
+        ('can_edit_own_profile',      ('A',          'A',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
+        ('can_see_own_bugs',          ('A',          'A',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
+        ('can_add_timeentry',         ('A',          'A',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
+        ('can_justify_wrongtime',     ('A',          'A',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
+        ('can_justify_late',          (' ',          'A',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
+        ('can_view_presence',         (' ',          'A',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
+        ('can_edit_presence',         (' ',          'A',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
+        ('can_view_task_pivot',       (' ',          'A',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
+        ('can_view_teams',            (' ',          'A',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
+        ('can_view_sprints',          (' ',          'A',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
+        ('client_stuff',              (' ',          'A',        ' ',           ' ',            ' ',        ' ',  'A',     )),
+        ('can_edit_sprints',          (' ',          ' ',        'A',           'A',            ' ',        ' ',  ' ',     )),
+        ('can_view_clients',          (' ',          ' ',        'A',           'A',            ' ',        ' ',  ' ',     )),
+        ('can_view_projects',         (' ',          ' ',        'A',           'A',            ' ',        ' ',  ' ',     )),
+        ('can_edit_clients',          (' ',          ' ',        'A',           ' ',            ' ',        ' ',  ' ',     )),
+        ('can_edit_projects',         (' ',          ' ',        'A',           ' ',            ' ',        ' ',  ' ',     )),
+        ('can_edit_teams',            (' ',          ' ',        ' ',           ' ',            ' ',        'A',  ' ',     )),
+        ('can_see_users_times',       (' ',          ' ',        ' ',           ' ',            ' ',        'A',  ' ',     )),
+        ('hr_stuff',                  (' ',          ' ',        ' ',           ' ',            ' ',        'A',  ' ',     )),
+        ('can_see_inactive_users',    (' ',          ' ',        ' ',           ' ',            ' ',        'A',  ' ',     )),
+        ('can_edit_users',            (' ',          ' ',        ' ',           ' ',            ' ',        'A',  ' ',     )),
+        ('can_times_reports',         (' ',          ' ',        ' ',           ' ',            'A',        ' ',  ' ',     )),
+
+        # ADMIN ONLY:
+        ('can_edit_config',           (' ',          ' ',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
+        ('can_edit_trackers',         (' ',          ' ',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
+        ('can_delete_projects',       (' ',          ' ',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
+        ('can_delete_clients',        (' ',          ' ',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
+        ('can_edit_users_timeentry',  (' ',          ' ',        ' ',           ' ',            ' ',        ' ',  ' ',     )),
+    )
+
+    @staticmethod
+    def generate(perms):
+        base = [
+            (Allow, Authenticated, ('view',)),
+            (Allow, 'g:cron', 'cron'),
+            (Allow, 'g:admin', ALL_PERMISSIONS),
+            ]
+        groups = perms[0][1]
+
+        perms = perms[1:]
+
+        result = {group: [] for group in groups}
+        for perm, checklist in perms:
+            for group, check in zip(groups, checklist):
+                # perm, group, check
+                if check == 'A':
+                    result[group].append(perm)
+                elif check != ' ':
+                    raise Exception('%s not supported' % check)
+
+        dynamic = []
+        for group, perms in result.iteritems():
+            dynamic.append((Allow, 'g:%s' % group, perms))
+
+        return base + dynamic
+
+
+    def __init__(self, request):
+        self.request = request
+
+Root.__acl__ = Root.generate(Root.PERMS)
