5ae431181b48befbb59a3d7d94f1e82201af37b4
Kornel Maleszka <kornel.maleszka@stxnext.pl>
2014-02-20 13:53:39 +0100

    bump version

--- db_migrations/0.9.7-0.9.9.sql
+++ db_migrations/0.9.7-0.9.9.sql
@@ -2,3 +2,4 @@ ALTER TABLE "user" DROP COLUMN "freelancer" RESTRICT;
 ALTER TABLE "user" DROP COLUMN "is_programmer" RESTRICT;
 ALTER TABLE "user" DROP COLUMN "is_graphic_designer" RESTRICT;
 ALTER TABLE "user" DROP COLUMN "is_frontend_developer" RESTRICT;
+ALTER TABLE "user" ADD COLUMN "start_work_experience" DATE DEFAULT NULL;
